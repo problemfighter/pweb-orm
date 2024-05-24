ce260c9188c2b86da6a12ec0ac6f0d22ffa56141
Touhid Mia <touhid@banglafighter.com>
2024-05-24 20:27:07 +0600

    Finalized the SaaS Resolver

--- pweb_orm/orm/pweb_saas.py
+++ pweb_orm/orm/pweb_saas.py
@@ -1,11 +1,15 @@
 from abc import ABC, abstractmethod
-from flask import g, request
+from flask import g, current_app
+
+
+class PWebSaaSConst:
+    TENANT_KEY = "tkey"
 
 
 class PWebSaaSTenantResolver(ABC):
 
     @abstractmethod
-    def get_tenant_key(self) -> str:
+    def get_tenant_key(self) -> str | None:
         pass
 
 
@@ -13,26 +17,25 @@ class PWebSaaS:
     tenantResolver: PWebSaaSTenantResolver = None
 
     @staticmethod
-    def resolve_tenant_key():
-        return request.args.get("tkey")
-
-    @staticmethod
-    def init_tenant_key():
-        tenant_key = None
-        if PWebSaaS.tenantResolver:
+    def init_tenant_key(tenant_key=None):
+        if tenant_key:
+            return tenant_key
+        elif PWebSaaS.tenantResolver:
             tenant_key = PWebSaaS.tenantResolver.get_tenant_key()
-        else:
-            tenant_key = PWebSaaS.resolve_tenant_key()
 
         if tenant_key:
             PWebSaaS.set_tenant_key(tenant_key)
 
+        return tenant_key
+
     @staticmethod
     def set_tenant_key(key: str):
-        g.pweb_saas = {"key": key}
+        g.pweb_saas = {PWebSaaSConst.TENANT_KEY: key}
+        current_app.add_to_context_data(key=PWebSaaSConst.TENANT_KEY, value=key)
 
     @staticmethod
     def get_tenant_key():
-        if "pweb_saas" in g and "key" in g.pweb_saas:
-            return g.pweb_saas["key"]
-        return None
+        if "pweb_saas" in g and PWebSaaSConst.TENANT_KEY in g.pweb_saas:
+            return g.pweb_saas[PWebSaaSConst.TENANT_KEY]
+        tenant_key = current_app.get_context_data(key=PWebSaaSConst.TENANT_KEY)
+        return PWebSaaS.init_tenant_key(tenant_key=tenant_key)
