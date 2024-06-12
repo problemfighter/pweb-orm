4327140572726cb6558823cf96f2277c608eb453
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-06-12 07:50:51 +0600

    Added return type

--- pweb_orm/common/pweb_saas_context.py
+++ pweb_orm/common/pweb_saas_context.py
@@ -7,6 +7,7 @@ class PWebSaaSContext(threading.Thread):
     _params: dict = None
     _tkey: str = None
     _method = None
+    _return = None
 
     def __init__(self, tkey: str, method):
         threading.Thread.__init__(self)
@@ -17,16 +18,18 @@ class PWebSaaSContext(threading.Thread):
         return self
 
     def __exit__(self, exc_type, exc_value, exc_tb):
-        self.join()
+        pass
 
     def run(self):
         with PWebRegistry.pweb_app.app_context():
             PWebSaaS.set_tenant_key(self._tkey)
             if self._method and self._params:
-                self._method(**self._params)
+                self._return = self._method(**self._params)
             elif self._method:
-                self._method()
+                self._return = self._method()
 
     def set_args_and_start(self, **kwargs):
         self._params = kwargs
         self.start()
+        self.join()
+        return self._return
