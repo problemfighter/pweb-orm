645ec62f9e06f455573b51c5d985e92c798455ca
H.M.Touhid Mia (HMTMCSE) <hmtm.cse@gmail.com>
2024-08-27 17:14:08 +0600

    Updated Version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def get_dependencies():
 
 setup(
     name='pweb-orm',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/banglafighter/pweb-orm',
     license='Apache 2.0',
     author='Bangla Fighter',
